9de20f72e84420e75131d79d35fc546a38579091
Justin Smith <justin.smith@thundertech.com>
2013-09-10 19:36:25 -0400

    HTML Setup

---
+++ scripts/site.js
@@ -0,0 +1,60 @@
+/*
+	Filename             screen-script.js
+	Detail               Screen functionality
+	Author:              thunder::tech inc.
+	License:             CLIENT is defined as the owner of online property from which this file resides or this code is referenced in.
+						 ADDITIONAL PARTY is defined as anyone other than thunder::tech or CLIENT.
+						 No right is granted to ADDITIONAL PARTY to sell, distribute, modify or otherwise transfer the following source code without explicit written permission by CLIENT or thunder::tech.
+
+	@codekit-prepend "thunder.js"
+	
+	Not in use:
+	
+	"polyfill.svg.js"
+	"polyfill.placeholder.js"
+	"polyfill.console.js"
+	"web.add-jsonp.js"
+	"web.auto-complete.js"
+	"web.constant-status.js"
+	"web.create-element.js"
+	"web.dispatcher.js"
+	"web.get-item-peers.js"
+	"web.get-property-map-of.js"
+	"web.get-query-value.js"
+	"web.item-browser.js"
+	"web.item-browser.define-presentation-as-scrolling-carousel.js"
+	"web.item-browser.define-presentation-as-scrolling-carousel.automate-presentation-using-touch.js"
+	"web.axes.js"
+	"web.menu-controller.js"
+	"web.menu-controller.get-levels-using-termed-ul-li.js"
+	"web.menu-controller.automate-items-using-click.js"
+	"web.menu-controller.automate-items-using-hover.js"
+	"web.menu-controller.automate-items-using-modernizr-click-and-hover.js"
+	"web.refresh-renderer.js"
+	"web.replace-image.js"
+	"web.round-to-place.js"
+	"web.scrolling-item-browser.js"
+	"web.set-prefixed-css.js"
+	"web.set-toggle-item-on.js"
+	"web.timed-queue.js"
+	"web.timer.js"
+	"web.user-messager.js"
+
+*/
+
+/*  ================================
+     Sitewide
+    ================================ */
+
+window.Project = new function() {
+
+	var _project;
+	_project = this;
+
+	function _onReady ( _event ) {
+		
+	}
+	
+	jQuery( _onReady );
+
+}();
